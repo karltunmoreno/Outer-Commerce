aa5b78b759da70b29e715ee16641a30137d5df36
unknown <Karltunmoreno@gmail.com>
2022-04-21 12:58:31 -0400

    e-commerce

---
+++ models/ProductTag.js
@@ -0,0 +1,39 @@
+// imports important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// imports our database connection from config.js
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
